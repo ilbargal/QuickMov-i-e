fdb9c865737aec68d2a00d42bb40fbacd53aedc5
Bar Gal <ilbargal@gmail.com>
2015-12-30 19:04:04 +0200

    Add movies fragment

--- app/src/main/java/com/finalandroidproject/quickmovie/MoviesActivity.java
+++ app/src/main/java/com/finalandroidproject/quickmovie/MoviesActivity.java
@@ -1,13 +1,49 @@
 package com.finalandroidproject.quickmovie;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.finalandroidproject.quickmovie.Fragments.MovieFragment;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class MoviesActivity extends Activity {
+
+    // Buttons
+    Button movieButton;
+    Button friendsButton;
+    Button invitationsButton;
+
+    // Fragments
+    MovieFragment movieFragment;
+
+    // Data Members
+    List<Fragment> fragmentsList = new LinkedList<Fragment>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies);
+
+        Initalize();
+
+        FragmentTransaction trans = getFragmentManager().beginTransaction();
+        trans.add(R.id.frgContainer, movieFragment);
+        trans.show(movieFragment);
+        // TODO : after this we need to add all fragments
+        trans.commit();
+    }
+
+    private void Initalize() {
+        movieButton = (Button) findViewById(R.id.btnMoviesTab);
+        friendsButton = (Button) findViewById(R.id.btnFriendsTab);
+        invitationsButton = (Button) findViewById(R.id.btnInvitationsTab);
+
+        movieFragment = new MovieFragment();
+        fragmentsList.add(movieFragment);
     }
 }
