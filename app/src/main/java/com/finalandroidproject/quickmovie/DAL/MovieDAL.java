3a4dfc946dfc6dd6578073f32b1cbf10d604b95d
nivg1992 <nivg1992.developer@gmail.com>
2016-01-16 18:07:27 +0200

    movieDAL Querys

--- app/src/main/java/com/finalandroidproject/quickmovie/DAL/MovieDAL.java
+++ app/src/main/java/com/finalandroidproject/quickmovie/DAL/MovieDAL.java
@@ -1,13 +1,18 @@
 package com.finalandroidproject.quickmovie.DAL;
 
+import android.util.Log;
+
 import com.finalandroidproject.quickmovie.Interfaces.IMovieActions;
 import com.finalandroidproject.quickmovie.Model.Movie;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
+import java.util.ArrayList;
 import java.util.Dictionary;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by nivg1 on 13/01/2016.
@@ -15,31 +20,114 @@ import java.util.List;
 public class MovieDAL implements IMovieActions {
     public final static MovieDAL instance = new MovieDAL();
 
-    @Override
-    public List<Movie> getAllMovies(int startIndex, int MaxMovies) {
-        // Parse get query for table Movies
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("Movies");
+    private boolean _movieInLocalStore = false;
 
+    @Override
+    public List<Movie> getAllMovies(int startIndex, int MaxMovies, boolean Refresh) {
         try {
+            // Parse get query for table Movies
+            ParseQuery<ParseObject> query = ParseQuery.getQuery("Movies");
+
+            if (_movieInLocalStore && !Refresh) {
+                query.fromLocalDatastore();
+            }
+
+            //query.include("Cinemas");
+
+            if(Refresh) {
+                ParseObject.unpinAllInBackground("Movies");
+            }
+
+            query.setLimit(MaxMovies);
+
             List<ParseObject> data = query.find();
 
+            List<Movie> arrMovies = new ArrayList<Movie>();
             for (ParseObject movieObject : data) {
+                if (!_movieInLocalStore) {
+                    movieObject.pinInBackground("Movies");
+                }
+
+                Movie mMovie = new Movie();
+                String name = movieObject.getString("Name");
+                String ganre = movieObject.getString("Ganre");
+                String description = movieObject.getString("Description");
+                String rating = movieObject.getString("Rating");
+                String image = movieObject.getString("Image");
+//                ParseObject Cinema = movieObject.getParseObject("Cinema");
+//                Log.d("Cinema",Cinema.getString("Name"));
+
+                mMovie = new Movie();
+                mMovie.setName(name);
+                mMovie.setGanre(ganre);
+                mMovie.setDescription(description);
+                mMovie.setRating(rating);
+                mMovie.setImagePath(image);
+
+                arrMovies.add(mMovie);
 
             }
-        } catch (ParseException e) {
 
+            return arrMovies;
+        } catch (ParseException e) {
+            return null;
         }
-        return null;
     }
 
     @Override
     public Movie getMovieByName(String movieName) {
-        return null;
+        HashMap<String, Object> parameters = new HashMap<>();
+        parameters.put("Name",movieName);
+        if(getMoviesByParameters(parameters).size() == 1){
+            return getMoviesByParameters(parameters).get(0);
+        } else {
+            return null;
+        }
+
     }
 
     @Override
-    public List<Movie> getMoviesByParameters(Dictionary<String, Object> parameters) {
-        return null;
+    public List<Movie> getMoviesByParameters(HashMap<String, Object> parameters) {
+        // Parse get query for table Users
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Movies");
+
+        for (Map.Entry<String, Object> param : parameters.entrySet()) {
+            query.whereEqualTo(param.getKey(), param.getValue());
+        }
+
+        if (_movieInLocalStore) {
+            query.fromLocalDatastore();
+        }
+
+        // Execute query (select)
+        List<ParseObject> data = null;
+
+        try {
+            data = query.find();
+
+            List<Movie> arrMovies = new ArrayList<Movie>();
+            for (ParseObject movieObject : data) {
+                Movie mMovie = new Movie();
+                String name = movieObject.getString("Name");
+                String ganre = movieObject.getString("Ganre");
+                String description = movieObject.getString("Description");
+                String rating = movieObject.getString("Rating");
+                String image = movieObject.getString("Image");
+
+                mMovie = new Movie();
+                mMovie.setName(name);
+                mMovie.setGanre(ganre);
+                mMovie.setDescription(description);
+                mMovie.setRating(rating);
+                mMovie.setImagePath(image);
+
+                arrMovies.add(mMovie);
+            }
+
+            return arrMovies;
+        } catch (ParseException e) {
+            return null;
+        }
     }
 
     @Override
